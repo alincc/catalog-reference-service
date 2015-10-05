84d4a15f4263f6b312d5286668be6da35b63393a
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-10-05 15:55:31 +0200

    BT-3375  - sonar removed unused imports

--- catalog-reference-service-rest/src/main/java/no/nb/microservices/catalogreference/core/reference/service/IReferenceService.java
+++ catalog-reference-service-rest/src/main/java/no/nb/microservices/catalogreference/core/reference/service/IReferenceService.java
@@ -1,9 +1,6 @@
 package no.nb.microservices.catalogreference.core.reference.service;
 
-import no.nb.microservices.catalogmetadata.model.mods.v3.Mods;
 import no.nb.microservices.catalogreference.model.Reference;
-
-import java.io.InputStream;
 
 public interface IReferenceService {
     Reference getWikipediaReference(String id);

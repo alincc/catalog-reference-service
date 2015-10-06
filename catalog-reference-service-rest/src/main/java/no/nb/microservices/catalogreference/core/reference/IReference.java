cfc2f7c0360c3eb23deffa59d66261c4d9c17c03
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-10-06 10:22:48 +0200

    BT-3375 - removed unused imports

--- catalog-reference-service-rest/src/main/java/no/nb/microservices/catalogreference/core/reference/IReference.java
+++ catalog-reference-service-rest/src/main/java/no/nb/microservices/catalogreference/core/reference/IReference.java
@@ -1,6 +1,5 @@
 package no.nb.microservices.catalogreference.core.reference;
 
-import no.nb.microservices.catalogmetadata.model.mods.v3.Mods;
 import no.nb.microservices.catalogreference.model.Reference;
 
 public interface IReference {

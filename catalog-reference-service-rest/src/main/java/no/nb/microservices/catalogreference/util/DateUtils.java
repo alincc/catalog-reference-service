91292344121726e4bd8a2fd3bc1819633ec8b185
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-10-19 13:04:39 +0200

    BT-3474  - added private constructor  - removed useless parentheses

--- catalog-reference-service-rest/src/main/java/no/nb/microservices/catalogreference/util/DateUtils.java
+++ catalog-reference-service-rest/src/main/java/no/nb/microservices/catalogreference/util/DateUtils.java
@@ -4,9 +4,13 @@ import org.apache.commons.validator.routines.DateValidator;
 
 public class DateUtils {
 
+    private DateUtils() {
+
+    }
+
     public static boolean isValidDate(String date) {
         DateValidator validator = DateValidator.getInstance();
-        return (validator.isValid(date,"yyyy") || validator.isValid(date,"yyyy-MM") || validator.isValid(date,"yyyy-MM-dd"));
+        return validator.isValid(date,"yyyy") || validator.isValid(date,"yyyy-MM") || validator.isValid(date,"yyyy-MM-dd");
     }
 
     public static String getRisAndEnwDate(String date) {
